c464fdfc82ef979a2271870008c5c86bbe2baaeb
Muhammad Hassan <hmuham7@vt.edu>
2015-12-10 23:27:31 +0000

    Changes

--- Twitter_input.py
+++ Twitter_input.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python
 import sys
 import socket
+import pickle
 import time
 import globals
 import RPi.GPIO as GPIO
@@ -17,6 +18,15 @@ OAUTH_TOKEN_SECRET = 'rFE5jrWsQ71ipO94fYtUwe292gGeU1HMJtIGlFuTkCTDQ'
 #Setting up global variable to be used later
 instruction = ''
 
+class Home:
+    def __init__(self): 
+        self.lights = False
+        self.fans = False
+        self.temp = 72
+
+    def report_status(self, client):
+        data = pickle.dumps(self)
+        client.send(data)
 
 # Setup callbacks from Twython Streamer
 class BlinkyStreamer(TwythonStreamer):
@@ -26,31 +36,61 @@ class BlinkyStreamer(TwythonStreamer):
                         str = data['text']
 
                         # Parsing mechanism
-                        strlist = str.split("_")
-                        instruction = strlist[0]
-			if len(strlist) > 1:
-				instruction =+ strlist[1]
+			if str == 'temp':
+				instruction = 'PI1 getStat'
+			else:
+				instruction = "PI1" + str
+			
+			host = globals.server_ip
+			port = 50000
+			size = 1024
+			s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+			s.connect((host,port))
+			sys.stdout.write('%')
 
-                        #Client side connection after receiving the IP and port number of Recipient RPi
-                        HOST = globals.server_ip    # The remote host
-                        PORT = 5000  # The same port as used by the server
-
-                        #Sockect initialization
-                        s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-                        s.connect((HOST, PORT))
-                        
+   
                         s.send(instruction)  # Sending the instruction to Recipient RPi
-                        data = s.recv(1024)
-                        #print 'Response from Server: ', repr(data)   #Receiving response from server
+                        data = s.recv(size)
+    
+			api = Twython(APP_KEY, APP_SECRET, OAUTH_TOKEN, OAUTH_TOKEN_SECRET)  # a new object for sending the response
+	
+			if instruction == 'getStat' or instruction == 'temp':
+    				print 'status of home'
+    				home = pickle.loads(data)
+    				print 'lights', home.lights
+    				print 'fans', home.fans
+    				print 'temp', home.temp
+				tweetStr = '@hmuham7 Status of Home: ' + str(home.lights) + str(home.fans) + str(home.temp)
+				api.update_status(status=tweetStr)
+    			
+			if instruction == 'lights ON' or instruction == 'lights OFF':
+				if data == 'ACK':
+					light = 'Lights status has been changed'
+					print light
+					tweetStr = '@hmuham7 ' + str(light)
+					api.update_status(status=tweetStr)
+				else:
+					light = 'Lights status is the same'
+                                        print light
+                                        tweetStr = '@hmuham7 ' + str(light)
+                                        api.update_status(status=tweetStr)
+			
+			if instruction == 'fans ON' or instruction == 'fans OFF':
+				if data == 'ACK':
+					fan = 'Fans status has been changed'
+					print fan
+					tweetStr = '@hmuham7 ' + str(fan)
+                                	api.update_status(status=tweetStr)
+				else:
+					fan = 'Fans status is the same'
+                                        print fan
+                                        tweetStr = '@hmuham7 ' + str(fan)
+                                        api.update_status(status=tweetStr)
+			
+			else:			
+    				sys.stdout.write(data)
+    				sys.stdout.write('%')
 
-                        api = Twython(APP_KEY, APP_SECRET, OAUTH_TOKEN, OAUTH_TOKEN_SECRET)  # a new object for sending the response
-
-
-                        #Sending the response tweet
-                        if data:
-                                tweetStr = '@hmuham7' + str
-                                api.update_status(status=tweetStr)
-                                print "Tweeted: " + tweetStr
                         s.close()
                         stream = BlinkyStreamer(APP_KEY, APP_SECRET, OAUTH_TOKEN, OAUTH_TOKEN_SECRET)
                         stream.statuses.filter(follow='2580010906')     # user ID is used to track users account for upcoming feeds.
