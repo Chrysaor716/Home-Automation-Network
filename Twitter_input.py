fcc1878bd4d2c3c22126f77fd51467b12fb15712
Muhammad Hassan <hmuham7@vt.edu>
2015-12-11 00:57:24 +0000

    Changes made

--- Twitter_input.py
+++ Twitter_input.py
@@ -32,21 +32,22 @@ class Home:
 class BlinkyStreamer(TwythonStreamer):
         def on_success(self, data):
                 if 'text' in data:
-                        print data['text'].encode('utf-8')
-                        str = data['text']
+                        #print data['text'].encode('utf-8')
+                        str_data = data['text']
 
                         # Parsing mechanism
-			if str == 'temp':
+			if str_data == 'temp':
 				instruction = 'PI1 getStat'
 			else:
-				instruction = "PI1" + str
-			
+				instruction = "PI1 " + str_data
+			print 'Instruction: ',instruction, '\n'
+	
 			host = globals.server_ip
 			port = 50000
 			size = 1024
 			s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 			s.connect((host,port))
-			sys.stdout.write('%')
+			#sys.stdout.write('%')
 
    
                         s.send(instruction)  # Sending the instruction to Recipient RPi
@@ -54,16 +55,28 @@ class BlinkyStreamer(TwythonStreamer):
     
 			api = Twython(APP_KEY, APP_SECRET, OAUTH_TOKEN, OAUTH_TOKEN_SECRET)  # a new object for sending the response
 	
-			if instruction == 'getStat' or instruction == 'temp':
-    				print 'status of home'
-    				home = pickle.loads(data)
-    				print 'lights', home.lights
-    				print 'fans', home.fans
-    				print 'temp', home.temp
-				tweetStr = '@hmuham7 Status of Home: ' + str(home.lights) + str(home.fans) + str(home.temp)
-				api.update_status(status=tweetStr)
+			if str_data == 'getStat' or str_data == 'temp':
+				if data:
+    					print 'Status of Home:'
+    					home = pickle.loads(data)
+    					
+					if home.lights == False:
+                                                light = 'OFF'
+                                        else:
+                                                light = 'ON'
+                                        if home.fans == False:
+                                                fan = 'OFF'
+                                        else:
+                                                fan = 'ON'
+					print 'Lights are:     ', light
+    					print 'Fans are:       ', fan
+    					print 'Temperature is: ', home.temp, 'C'
+					print '\n'
+					
+					tweetStr = '@hmuham7 Status of Home: ' + 'Lights are ' + str(light) + ', Fans are ' + str(fan) + ' and Temperature is ' + str(home.temp) + 'C'
+					api.update_status(status=tweetStr)
     			
-			if instruction == 'lights ON' or instruction == 'lights OFF':
+			if str_data == 'lights ON' or str_data == 'lights OFF':
 				if data == 'ACK':
 					light = 'Lights status has been changed'
 					print light
@@ -75,7 +88,7 @@ class BlinkyStreamer(TwythonStreamer):
                                         tweetStr = '@hmuham7 ' + str(light)
                                         api.update_status(status=tweetStr)
 			
-			if instruction == 'fans ON' or instruction == 'fans OFF':
+			if str_data == 'fans ON' or str_data == 'fans OFF':
 				if data == 'ACK':
 					fan = 'Fans status has been changed'
 					print fan
@@ -87,9 +100,9 @@ class BlinkyStreamer(TwythonStreamer):
                                         tweetStr = '@hmuham7 ' + str(fan)
                                         api.update_status(status=tweetStr)
 			
-			else:			
-    				sys.stdout.write(data)
-    				sys.stdout.write('%')
+			#else:			
+    			#	sys.stdout.write(data)
+    			#	sys.stdout.write('%')
 
                         s.close()
                         stream = BlinkyStreamer(APP_KEY, APP_SECRET, OAUTH_TOKEN, OAUTH_TOKEN_SECRET)
@@ -101,6 +114,6 @@ try:
         stream.statuses.filter(follow='2580010906')
 
 except KeyboardInterrupt:
-        print 'Interface has been Terminated Manually'
+        print '\nInterface has been Terminated Manually'
 
 
